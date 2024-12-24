d20cfca0cb309ddb41383aa55dadac05c1c89b47
Mukesh Tiwari <mukeshtiwari.iiitm@gmail.com>
2024-12-24 13:15:24 +0000

    Independent generators

--- chapter-01.py
+++ chapter-01.py
@@ -6,28 +6,26 @@ from libnum import has_sqrtmod_prime_power, sqrtmod_prime_power
 
 b = 3 # for bn128, y^2 = x^3 + 3
 seed = "RareSkills"
-
-x = int(sha256(seed.encode('ascii')).hexdigest(), 16) % field_mod 
-
-entropy = 0
-
+x = int(sha256(seed.encode('ascii')).hexdigest(), 16) % field_mod
 vector_basis = []
 
 # modify the code below to generate n points
+# on the curve y^2 = x^3 + b
+n = 10
 
-n = 4
+while len(vector_basis) < n:
+    entropy = 0  
+    while True : 
+        x = (x + entropy) % field_mod
+        y_squared = (x**3 + b) % field_mod
+        if has_sqrtmod_prime_power(y_squared, field_mod, 1):
+            y = list(sqrtmod_prime_power(y_squared, field_mod, 1))[entropy % 2]
+            vector_basis.append((FQ(x), FQ(y)))
+            assert is_on_curve(vector_basis[-1], b)
+            break
+        entropy += 1
+        
+    x = int(sha256(str(x).encode('ascii')).hexdigest(), 16) % field_mod
 
-while not has_sqrtmod_prime_power((x**3 + b) % field_mod, field_mod, 1):
-    # increment x, so hopefully we are on the curve
-    x = (x + 1) % field_mod
-    entropy = entropy + 1
 
-# pick the upper or lower point depending on if entropy is even or odd
-y = list(sqrtmod_prime_power((x**3 + b) % field_mod, field_mod, 1))[entropy & 1 == 0]
-point = (FQ(x), FQ(y))
-assert is_on_curve(point, b), "sanity check"
-vector_basis.append(point)
-
-# new x value
-x = int(sha256(str(x).encode('ascii')).hexdigest(), 16) % field_mod 
 print(vector_basis)
